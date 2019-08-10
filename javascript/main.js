6a515d0c401ff4f8ed00cef12caf4bca3c8faf68
Radiscipio <robertdiscipio@gmail.com>
2019-08-10 10:11:45 -0400

    Added scroll to element

--- javascript/main.js
+++ javascript/main.js
@@ -1,7 +1,13 @@
 // API Card Call
 
-let originalURL = 'https://docs.google.com/spreadsheets/d/1Xsouj8zOSQHX0Y-iL3IvBJVg55WxV0FOzMDVx-8sdtg/edit#gid=0'
+// let originalURL = 'https://docs.google.com/spreadsheets/d/1Xsouj8zOSQHX0Y-iL3IvBJVg55WxV0FOzMDVx-8sdtg/edit#gid=0'
+//
+// let id = '1Xsouj8zOSQHX0Y-iL3IvBJVg55WxV0FOzMDVx-8sdtg'
+//
+// let source = `https://spreadsheets.google.com/feeds/list/${id}/od6/public/values?alt=json`
+let skillsButton = document.getElementById('skills')
 
-let id = '1Xsouj8zOSQHX0Y-iL3IvBJVg55WxV0FOzMDVx-8sdtg'
-
-let source = `https://spreadsheets.google.com/feeds/list/${id}/od6/public/values?alt=json`
+skillsButton.addEventListener('click', function() {
+  let skills = document.querySelector('#skills-page')
+  skills.scrollIntoView()
+})
