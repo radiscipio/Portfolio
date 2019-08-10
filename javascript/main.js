d6da612fea011c20f5a4dab1e064d6077633afb3
Radiscipio <robertdiscipio@gmail.com>
2019-08-10 15:46:06 -0400

    Started on API image carousel

--- javascript/main.js
+++ javascript/main.js
@@ -1,7 +1,7 @@
 // API Card Call
 
-// let originalURL = 'https://docs.google.com/spreadsheets/d/1Xsouj8zOSQHX0Y-iL3IvBJVg55WxV0FOzMDVx-8sdtg/edit#gid=0'
-//
-// let id = '1Xsouj8zOSQHX0Y-iL3IvBJVg55WxV0FOzMDVx-8sdtg'
-//
-// let source = `https://spreadsheets.google.com/feeds/list/${id}/od6/public/values?alt=json`
+let originalURL = 'https://docs.google.com/spreadsheets/d/1Xsouj8zOSQHX0Y-iL3IvBJVg55WxV0FOzMDVx-8sdtg/edit#gid=0'
+
+let id = '1Xsouj8zOSQHX0Y-iL3IvBJVg55WxV0FOzMDVx-8sdtg'
+
+let source = `https://spreadsheets.google.com/feeds/list/${id}/od6/public/values?alt=json`
